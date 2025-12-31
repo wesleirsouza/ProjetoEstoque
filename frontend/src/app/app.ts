f43a688980a4536eb62c4439aca1bf17f50b57c9
WesleiRodriguesSouza <souzawesleirodrigues@gmail.com>
2025-12-30 21:00:21 -0300

    feat: adicionando menu lateral utilizando router e melhorando a tela principal

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
+import { Sidebar } from "./components/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [RouterLink],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss',
+})
+export class Sidebar {
+
+}
