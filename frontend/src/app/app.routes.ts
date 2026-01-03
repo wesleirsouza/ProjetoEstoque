fe7bc634957217c97836a88a4921509f83f6a0d2
WesleiRodriguesSouza <souzawesleirodrigues@gmail.com>
2026-01-03 20:56:49 -0300

    feat: adicionando a rota no route e adicionando no sidebar o routerLink para products

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { ProductsList } from './components/products-list/products-list';
 
 export const routes: Routes = [
     { path: '', component: Home, title: 'Estoque' },
-    
+    { path: 'products', component: ProductsList, title: 'produto'}
 ];
